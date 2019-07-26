6138285582152f8c5d9905b20491f26d01b0fa7c
macro <ZHANGQUAN526@pingan.com.cn>
2019-07-26 15:16:25 +0800

    add example

---
+++ Example/ZQFoundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZQFoundation
+//
+//  Created by macro on 11/15/2018.
+//  Copyright (c) 2018 macro. All rights reserved.
+//
+
+@import UIKit;
+#import "ZQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZQAppDelegate class]));
+    }
+}
