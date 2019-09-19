0193c39acaedce3bf8aa06f149cc84ad2c4bc11d
macro <ZHANGQUAN526@pingan.com.cn>
2019-09-19 17:09:27 +0800

    update

--- ZQCommon/ZQShareCode.h
+++ ZQCommon/ZQShareCode.h
@@ -6,6 +6,20 @@
 //  Copyright © 2018年 Macro. All rights reserved.
 //
 
+#if __has_include(<ZQFoundation/ZQShareCode.h>)
+
+#import <ZQFoundation/ZQMacros.h>
+#import <ZQFoundation/ZQSingleton.h>
+
+#import <ZQFoundation/NSArray+ZQAdd.h>
+#import <ZQFoundation/NSDictionary+ZQAdd.h>
+#import <ZQFoundation/NSObject+ZQAdd.h>
+#import <ZQFoundation/NSString+ZQAdd.h>
+#import <ZQFoundation/NSTimer+ZQBlock.h>
+#import <ZQFoundation/UIColor+ZQAdd.h>
+#import <ZQFoundation/UIImage+ZQAdd.h>
+
+#else
 
 #import "ZQMacros.h"
 #import "ZQSingleton.h"
@@ -17,5 +31,7 @@
 #import "NSTimer+ZQBlock.h"
 #import "UIColor+ZQAdd.h"
 #import "UIImage+ZQAdd.h"
+
+#endif
 
 
--- ZQUIKit/ZQUIKit.h
+++ ZQUIKit/ZQUIKit.h
@@ -6,14 +6,31 @@
 //
 
 
+#if __has_include(<ZQFoundation/ZQUIKit.h>)
+
+#import <ZQFoundation/UIButton+ZQAdd.h>
+#import <ZQFoundation/UIView+ZQExten.h>
+#import <ZQFoundation/UIViewController+ZQCustomizeNavBar.h>
+
+#import <ZQFoundation/BaseModel.h>
+#import <ZQFoundation/BaseViewModel.h>
+#import <ZQFoundation/BaseViewController.h>
+#import <ZQFoundation/BaseTableViewController.h>
+#import <ZQFoundation/BaseTableViewCell.h>
+#import <ZQFoundation/BaseTableViewAdaptor.h>
+
+#else
 
 #import "UIButton+ZQAdd.h"
 #import "UIView+ZQExten.h"
 #import "UIViewController+ZQCustomizeNavBar.h"
 
 #import "BaseModel.h"
+#import "BaseViewModel.h"
 #import "BaseViewController.h"
 #import "BaseTableViewController.h"
 #import "BaseTableViewCell.h"
 #import "BaseTableViewAdaptor.h"
-#import "BaseViewModel.h"
+
+#endif
+
