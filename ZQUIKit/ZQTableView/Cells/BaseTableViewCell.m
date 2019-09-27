b894af232c487494cbf84c773070ca30522c5007
macro <ZHANGQUAN526@pingan.com.cn>
2019-09-27 15:16:47 +0800

    update

--- ZQUIKit/ZQTableView/Cells/BaseTableViewCell.m
+++ ZQUIKit/ZQTableView/Cells/BaseTableViewCell.m
@@ -17,6 +17,14 @@ static const CGFloat DefaultCellHeight = 44.0f;
 
 @implementation BaseTableViewCell
 
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    //XIB 初始化设置同手动初始化
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
+    self.separatorInset = UIEdgeInsetsMake(0,0,0,viewWidth);
+    self.accessoryType = UITableViewCellAccessoryNone;
+}
+
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
